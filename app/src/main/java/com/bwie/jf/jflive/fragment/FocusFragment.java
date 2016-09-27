f8e32261375fc4bb133e06a34bfff5bc0de382e6
Lee <304394048@qq.com>
2016-09-27 11:38:06 +0800

    add Fragment

---
+++ app/src/main/java/com/bwie/jf/jflive/fragment/FocusFragment.java
@@ -0,0 +1,26 @@
+package com.bwie.jf.jflive.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bwie.jf.jflive.R;
+
+/**
+ * Created by Lee_peng on 2016/9/27.
+ */
+public class FocusFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.vpager_page1,container,false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
