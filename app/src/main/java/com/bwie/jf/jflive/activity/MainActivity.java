a1480dacfa66ada37dff5ab1e987f347eec96ae9
Your Name <you@example.com>
2016-09-28 15:22:56 +0800

    第一次主页提交

--- app/src/main/java/com/bwie/jf/jflive/activity/MainActivity.java
+++ app/src/main/java/com/bwie/jf/jflive/activity/MainActivity.java
@@ -1,15 +1,74 @@
 package com.bwie.jf.jflive.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RelativeLayout;
 
 import com.bwie.jf.jflive.R;
+import com.bwie.jf.jflive.fragment.HomePageFragment;
+import com.bwie.jf.jflive.fragment.MineFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
+    private RadioButton rb_1,rb_2;
+    private RelativeLayout relout;
+    private HomePageFragment homepage;
+    private MineFragment mine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //初始化
+        init();
+    }
+
+    private void init() {
+        rb_1= (RadioButton) findViewById(R.id.main_rb_1);
+        rb_2= (RadioButton) findViewById(R.id.main_rb_2);
+        relout= (RelativeLayout) findViewById(R.id.main_relout);
+
+        rb_1.setOnClickListener(this);
+        rb_2.setOnClickListener(this);
+        //显示隐藏fragment并提交
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        homepage=new HomePageFragment();
+        mine=new MineFragment();
+        ft.add(R.id.main_relout,homepage,"homepage").show(homepage).add(R.id.main_relout,mine,"mine").hide(mine);
+        ft.commit();
+    }
+
+    //radiogroup监听事件
+    @Override
+    public void onClick(View v) {
+        int id=v.getId();
+        switch (id){
+            case R.id.main_rb_1:
+                FragmentManager fm1=getSupportFragmentManager();
+                FragmentTransaction ft1 = fm1.beginTransaction();
+                ft1.show(homepage).hide(mine).commit();
+                rb_1.setTextColor(Color.RED);
+                rb_1.setChecked(true);
+                rb_2.setChecked(false);
+                rb_2.setTextColor(Color.BLACK);
+                break;
+            case R.id.main_rb_2:
+                FragmentManager fm2=getSupportFragmentManager();
+                FragmentTransaction ft2 = fm2.beginTransaction();
+                ft2.show(mine).hide(homepage).commit();
+                rb_2.setTextColor(Color.RED);
+                rb_2.setChecked(true);
+                rb_1.setChecked(false);
+                rb_1.setTextColor(Color.BLACK);
+                break;
+            default :
+
+                break;
+        }
     }
 }
--- app/src/main/java/com/bwie/jf/jflive/fragment/HomePageFragment.java
+++ app/src/main/java/com/bwie/jf/jflive/fragment/HomePageFragment.java
@@ -1,26 +1,114 @@
 package com.bwie.jf.jflive.fragment;
 
+import android.annotation.TargetApi;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.bwie.jf.jflive.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Lee_peng on 2016/9/26.
  */
 public class HomePageFragment extends Fragment {
+    private RadioGroup rg;
+    private ViewPager vp;
+    private List<Fragment> list;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
-
+        rg= (RadioGroup) view.findViewById(R.id.home_rg);
+        vp= (ViewPager) view.findViewById(R.id.home_vp);
         return view;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        //franment添加到list集合
+        fragmentAdd();
+        //viewpager的适配
+        viewpagerAdapter();
+        //viewpager的监听
+        viewpagerListener();
+        //RadioButton的监听
+        for(int i=0;i<list.size();i++){
+            RadioButton rb= (RadioButton) rg.getChildAt(i);
+            final int j=i;
+            rb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @TargetApi(Build.VERSION_CODES.DONUT)
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    vp.setCurrentItem(j);
+                }
+            });
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.DONUT)
+    private void viewpagerListener() {
+        vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for(int i=0;i<list.size();i++){
+                    RadioButton rb= (RadioButton) rg.getChildAt(i);
+                    if(position==i){
+                        rb.setChecked(true);
+                        rb.setTextColor(Color.RED);
+                    }else{
+                        rb.setChecked(false);
+                        rb.setTextColor(Color.BLACK);
+                    }
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    @TargetApi(Build.VERSION_CODES.DONUT)
+    private void viewpagerAdapter() {
+        vp.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return list.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return list!=null?list.size():0;
+            }
+        });
+    }
+
+    private void fragmentAdd() {
+        list=new ArrayList<Fragment>();
+        FocusFragment focus=new FocusFragment();
+        HotFragment hot=new HotFragment();
+        NewestFragment news=new NewestFragment();
+
+        list.add(focus);
+        list.add(hot);
+        list.add(news);
     }
 }
